e686a423870ebe34cdba5584ea35cca7211e521c
Arun <arun.parameswaran@infosys.com>
2020-04-06 22:16:11 +0530

    Initial Commit

---
+++ ToDoListApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ToDoListApp
+//
+//  Created by Arun Kappattiparambil Parameswaran on 06/04/20.
+//  Copyright © 2020 Arun Kappattiparambil Parameswaran. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
